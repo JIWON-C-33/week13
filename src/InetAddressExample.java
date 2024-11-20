4bd51c4cd3d103f6ddb51a36e8859cdec2b3c128
천지원 <202430331@email.daelim.ac.kr>
2024-11-20 16:46:53 +0900

    Initial commit

---
+++ src/InetAddressExample.java
@@ -0,0 +1,18 @@
+import java.net.*;
+
+public class InetAddressExample {
+    public static void main(String[] args) throws Exception {
+        InetAddress local = InetAddress.getLocalHost();
+        System.out.println("Local Host: " + local);
+        System.out.println("Local HostAddress: " + local.getHostAddress());
+        System.out.println("Local HostName: " + local.getHostName());
+
+        InetAddress google = InetAddress.getByName("www.google.com");
+        System.out.println("GOOGLE IP: " + google);
+
+        InetAddress[] addresses = InetAddress.getAllByName("www.google.com");
+        for (InetAddress addr : addresses) {
+            System.out.println("GOOGLE Address: " + addr);
+        }
+    }
+}
